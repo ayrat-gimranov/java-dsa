a1d16985ff102ed36bd66bad59f9412e744beec5
Ayrat Gimranov <gimranov45@gmail.com>
2022-10-04 09:04:40 -0700

    Lab 1.1 solved

--- Lab1.1_Starter/src/com/algorithms/BinaryConvert.java
+++ Lab1.1_Starter/src/com/algorithms/BinaryConvert.java
@@ -1,14 +1,33 @@
 package com.algorithms;
 
 public class BinaryConvert {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int number = convertFromBinary("100101");
         System.out.println("Number for binary 100101: " + number);
     }
 
-    public static int convertFromBinary(String binary){
+    public static int convertFromBinary(String binary) {
         // TODO-Lab1.1: return calculated decimal value converted from String binary
-        return 0;
+        // loop the string from the bottom of the string
+        // - get the last character, and check if it is 1 or not
+        // - if it is 1, compute the value of that digit with the conversion
+        //      - add it to the result
+        // - change the conversion by the power of 2
+
+        int result = 0;
+        int conversion = 1;
+
+        for (int i = binary.length() - 1; i >= 0; i--) {
+            int num = Integer.parseInt(String.valueOf(binary.charAt(i)));
+            if (num == 1) {
+                num = num * conversion;
+                result = result + num;
+            }
+            conversion = conversion * 2;
+        }
+
+        return result;
     }
 
 }
+
---
+++ my-project/src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello today is day 2");
+    }
+}
